6179fdef2b906e37f22d79f4fbd0af787d5b7155
Keliomer Castillo <keliomer@gmail.com>
2016-04-07 23:34:37 -0400

    Initial Commit

---
+++ RFC_downloader.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  RFC_downloader.py
+#  
+#  Copyright 2016 Keliomer Castillo <keliomer@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import sys, urllib.request
+
+try:
+    rfc_number = int(sys.argv[1])
+except (IndexError, ValueError):
+    print('Must supply an RFC number as the first argument!')
+    sys.exit(2)
+
+template = 'http://www.ietf.org/rfc/rfc{}.txt'
+url = template.format(rfc_number)
+rfc_raw = urllib.request.urlopen(url).read()
+rfc = rfc_raw.decode()
+print(rfc)
