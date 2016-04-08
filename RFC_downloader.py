192d043fc65b6dbb8454a2e49a120985d8cac29d
Keliomer Castillo <keliomer@gmail.com>
2016-04-07 23:50:07 -0400

    TCP/UDP layer version'

--- RFC_downloader.py
+++ RFC_downloader.py
@@ -22,7 +22,7 @@
 #  
 #  
 
-import sys, urllib.request
+import sys, socket
 
 try:
     rfc_number = int(sys.argv[1])
@@ -30,8 +30,29 @@ except (IndexError, ValueError):
     print('Must supply an RFC number as the first argument!')
     sys.exit(2)
 
-template = 'http://www.ietf.org/rfc/rfc{}.txt'
-url = template.format(rfc_number)
-rfc_raw = urllib.request.urlopen(url).read()
-rfc = rfc_raw.decode()
+host = 'www.ietf.org'
+port = 80
+sock = socket.create_connection((host,port))
+
+req = (
+    'GET /rfc/rfc{rfcnum}.txt HTTP/1.1\r\n'
+    'Host:{host}:{port}\r\n'
+    'User-Agent: Python {version}\r\n'
+    '\r\n'
+    )
+    
+req = req.format(
+    rfcnum=rfc_number,
+    host=host,
+    port=port,
+    version=sys.version_info[0]
+    )
+sock.sendall(req.encode('ascii'))
+rfc_raw = bytearray()
+while True:
+    buf = sock.recv(4096)
+    if not len(buf):
+        break
+    rfc_raw += buf
+rfc = rfc_raw.decode('utf-8')
 print(rfc)
